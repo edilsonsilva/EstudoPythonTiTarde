8c1fe889c921ad6b9cf4c9d92c1b6e05efaba4f6
Edilson Silva <edilson@yahoo.com.br>
2024-03-01 14:18:31 -0300

    Aula02-primeiro arquivo match

---
+++ Aula02/match1.py
@@ -0,0 +1,22 @@
+print("Este programa analisa os valores digitado de 0 a 6 e diz o dia da semana")
+digito = input("Entre com um número de 0 a 6")
+
+match digito:
+    case '0':
+        print("Domingo")
+    case '1':
+        print("Segunda-Feira")
+    case '2':
+        print("Terça-Feira")
+    case '3':
+        print("Quarta-Feira")
+    case '4':
+        print("Quinta-Feira")
+    case '5':
+        print("Sexta-Feira")
+    case '6':
+        print("Sábado")
+    case _:
+        print("Valor incorreto. Tente outra vez")
+    
+        
